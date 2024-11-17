d9e3891226ee9008d1575e6d790a940804bc2dfe
Rittidate Kit <rittidate.kit@truedigital.com>
2024-11-17 20:34:44 +0700

    access element programatically

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -18,8 +18,10 @@ export class ControlComponent {
   //   console.log('click')
   // }
   label = input.required<string>();
+  private el = inject(ElementRef);
 
   onClick() {
-    console.log('Clicked!')
+    console.log('Clicked!');
+    console.log(this.el);
   }
 }
