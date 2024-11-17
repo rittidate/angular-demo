3de70d52e433dae16deb15c7d75cab2ce69e73c1
Rittidate Kit <rittidate.kit@truedigital.com>
2024-11-17 12:17:16 +0700

    complete service signal

--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
 import { InvestmentInput } from "./investment-input.model";
 
 @Injectable({ providedIn: 'root'})
 export class InvestmentService {
-    resultsData?: {
+    resultsData = signal<{
         year: number;
         interest: number;
         valueEndOfYear: number;
         annualInvestment: number;
         totalInterest: number;
         totalAmountInvested: number;
-    }[];
+      }[] | undefined>(undefined);
 
     onCulateInvestmentResults(data: InvestmentInput) {
         const {
@@ -37,6 +37,6 @@ export class InvestmentService {
             totalAmountInvested: initialInvestment + annualInvestment * year,
         });
         }
-        this.resultsData = annualData;
+        this.resultsData.set(annualData);
     }
 }
