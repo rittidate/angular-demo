3d2af3c2ea177473911a3dc93cf85d615e09ca0c
Rittidate Kit <rittidate.kit@truedigital.com>
2024-11-17 17:31:47 +0700

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,19 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { DashboardStatusComponent } from './dashboard-status/dashboard-status.component';
+import { DashboardTrafficComponent } from './dashboard-traffic/dashboard-traffic.component';
+
+import { DashboardTicketComponent } from './dashboard-ticket/dashboard-ticket.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [HeaderComponent,
+      DashboardStatusComponent,
+      DashboardTrafficComponent,
+      DashboardTicketComponent]
 })
 export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
+
 }
---
+++ src/app/dashboard-ticket/dashboard-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-ticket.component.html',
+  styleUrl: './dashboard-ticket.component.css'
+})
+export class DashboardTicketComponent {
+
+}
